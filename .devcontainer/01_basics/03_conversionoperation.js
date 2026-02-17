ad4121e5830608000fedb8628775d1eab66e810b
vetny001 <109505737+vetny001@users.noreply.github.com>
2026-02-17 15:59:23 +0000

    coversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,18 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => output as Nan not a number 
+// true => 1; false => 0; 
+
+let isloggedIn = 1
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 
+// "" => false; 
+
