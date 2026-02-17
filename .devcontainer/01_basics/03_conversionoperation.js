c5b3106ce03c7be3ebc470832f7bf0f782c8bdc1
vetny001 <109505737+vetny001@users.noreply.github.com>
2026-02-17 16:19:46 +0000

    commit

--- .devcontainer/01_basics/03_conversionoperation.js
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -1,9 +1,9 @@
 let score = "33abc"
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(valueInNumber);
+//console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => output as Nan not a number 
@@ -11,8 +11,23 @@ console.log(valueInNumber);
 
 let isloggedIn = 1
 let booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 
 // "" => false; 
 
+// ********************** Operations ************************** // 
+
+let value = 3 
+let negValue = -value
+//console.log(negValue); 
+
+//tricky things
+
+let str1 = "hello"
+let str2 = " nikhil"
+let str3 = str1 + str2
+console.log(str3);
+
+//link to study more
+//
